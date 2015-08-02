ddecad0e8d02ea5393e677670d7eb170a84fd136
Rafal <rafal.araszkiewicz@student.put.poznan.pl>
2015-08-02 18:22:52 +0200

    Completed Roster class.

--- RozgrywkiLigiKoszykowki/RozgrywkiLigiKoszykowki.Windows/Model/Roster.cs
+++ RozgrywkiLigiKoszykowki/RozgrywkiLigiKoszykowki.Windows/Model/Roster.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace RozgrywkiLigiKoszykowki.Model
 {
+    // Roster means "harmonogram dyżurów"
     class Roster
     {
+        /// <summary>
+        /// Team name
+        /// </summary>
+        public string TeamName { get; private set; }
+
+        /// <summary>
+        /// List of players in team.
+        /// </summary>
+        private readonly List<Player> _players = new List<Player>();
+
+        /// <summary>
+        /// Returns reference to new copy of players list
+        /// </summary>
+        public IEnumerable<Player> Players
+        {
+            get { return new List<Player>(_players); }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="teamName">Team name</param>
+        /// <param name="players">List of players in team</param>
+        public Roster(string teamName, IEnumerable<Player> players)
+        {
+            TeamName = teamName;
+            _players.AddRange(players);
+        }
     }
 }
